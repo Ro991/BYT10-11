68fd32bdb175e07e74e0684e23cd0123fc35f6fd
alice-lashuk <alice.lashuk@gmail.com>
2020-12-22 18:44:41 +0100

    Trip and Route added

---
+++ src/Route.java
@@ -0,0 +1,7 @@
+public class Route {
+    private Transportation methodOfTransportation;
+    private Location destination;
+    private Location origin;
+    private LocalDate arrival;
+    private LocalDate departure;
+}
---
+++ src/Trip.java
@@ -0,0 +1,68 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Trip {
+    private ArrayList<Route> routes;
+    private LocalDate startDateTime;
+    private LocalDate endDateTime;
+    private Location origin;
+    private Location finalDestination;
+    private ArrayList<Accommodation> accommodation;
+
+    public BigDecimal tripCost() {
+        return null;
+    }
+
+    public static Trip createSchedule() {
+        return null;
+    }
+
+    public ArrayList<Accommodation> getAccommodation() {
+        return accommodation;
+    }
+
+    public LocalDate getEndDateTime() {
+        return endDateTime;
+    }
+
+    public ArrayList<Route> getRoutes() {
+        return routes;
+    }
+
+    public LocalDate getStartDateTime() {
+        return startDateTime;
+    }
+
+    public Location getFinalDestination() {
+        return finalDestination;
+    }
+
+    public Location getOrigin() {
+        return origin;
+    }
+
+    public void setEndDateTime(LocalDate endDateTime) {
+        this.endDateTime = endDateTime;
+    }
+
+    public void setFinalDestination(Location finalDestination) {
+        this.finalDestination = finalDestination;
+    }
+
+    public void setOrigin(Location origin) {
+        this.origin = origin;
+    }
+
+    public void setStartDateTime(LocalDate startDateTime) {
+        this.startDateTime = startDateTime;
+    }
+
+    public void addRoute() {
+
+    }
+
+    public void addAccommodation() {
+
+    }
+}
