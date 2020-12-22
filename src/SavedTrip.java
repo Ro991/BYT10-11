d674447213514229b20102a97419cf9cc34a35b1
patjagielski <jagielskipat@gmail.com>
2020-12-22 19:20:09 +0100

    added SavedTrip and User information

---
+++ src/SavedTrip.java
@@ -0,0 +1,31 @@
+public class SavedTrip {
+    private int tripId;
+    private String tripName;
+
+    public SavedTrip(int tripId, String tripName){
+        this.tripId = tripId;
+        this.tripName = tripName;
+    }
+
+    public int getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(int tripId) {
+        this.tripId = tripId;
+    }
+
+    public String getTripName() {
+        return tripName;
+    }
+
+    public void setTripName(String tripName) {
+        this.tripName = tripName;
+    }
+    public Trip editTrip(Trip trip){
+
+    }
+    public void deleteTrip(Trip selectedTrip){
+
+    }
+}
