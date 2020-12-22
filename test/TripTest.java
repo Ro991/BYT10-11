eb3ba38eedd8a3c149fc45c64f6b66cce37fd7f5
Ro991 <risayenko@gmail.com>
2020-12-22 19:53:48 +0100

    sample test

---
+++ test/TripTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TripTest {
+
+    @Test
+    void getStartDateTime() {
+    }
+
+    @Test
+    void setStartDateTime() {
+    }
+
+    @Test
+    void Trip(){
+
+    }
+}
